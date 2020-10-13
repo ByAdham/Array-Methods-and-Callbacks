f2d160e1657d18886b1d3588e3d63059dcb41c73
Adham AbdelFattah <adham.adel@gmail.com>
2020-10-13 17:50:33 -0500

    finished task 3 and started task 4

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ console.log(getYears(getFinals, fifaData));
 
 /* Task 4: Implement a higher-order function called `getWinners`, that accepts the callback function `getFinals()` and determine the winner (home or away) of each `finals` game. Return the name of all winning countries in an array called `winners` */ 
 
-function getWinners(/* code here */) {
+function getWinners(cb, data) {
 
     /* code here */
 
